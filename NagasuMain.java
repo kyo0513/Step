b2e34851ef4aa58dc9e88c6865d8233a4edd9c1b
kyo0513 <tyrant-zero-132127@docomo.ne.jp>
2022-03-28 13:22:59 +0900

    X追加

--- NagasuMain.java
+++ NagasuMain.java
@@ -1,13 +1,23 @@
 import java.util.*;
 public class NagasuMain{
 	public static void main(String[] args){
-		System.out.print("何段 >>");
+		System.out.print("何段>>");
 		int row = new Scanner(System.in).nextInt();
 		for(int i = 0;i < row ; i++){
-			for(int j = 0 ; j < i +1 ; j++){
+			for(int j = 0 ; j < i + 1 ; j++){
 				System.out.print("*");
 			}
 			System.out.println();
+		}
+		System.out.println("//逆");
+		for(int i = 0;i < row ; i++){
+			for(int j = 0; j < row ; j++){
+				System.out.printf(j < i ? " ":"*");
+			}
+			System.out.println();
+		}
+		for(int i =0;i < 3;i++){
+			System.out.printf(i%2 == 0 ? "X X%n" : " X %n");
 		}
 	}
 }
